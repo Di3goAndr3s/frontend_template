92fbc2c6f719c8ab3aac7d73d726ec99b75154a7
Di3goAndr3s <diego@imaginamos.co>
2014-03-12 13:51:34 -0500

    0.8

--- app/index.js
+++ app/index.js
@@ -4,18 +4,18 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 
 
-var NGenerator = module.exports = function NGenerator(args, options, config) {
+var ImaginamosGenerator = module.exports = function NGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
   var GeneratorSettings = require('./settings');
-  var settingsFileName = path.join(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE, '.yo-n.conf');
+  var settingsFileName = path.join(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE, '.yo-imaginamos.conf');
   this.settings = new GeneratorSettings(settingsFileName);
 };
 
 util.inherits(NGenerator, yeoman.generators.Base);
 
-NGenerator.prototype.npmInit = require('./npmInit.js');
+//NGenerator.prototype.npmInit = require('./npmInit.js');
 
-NGenerator.prototype.buildFilesFromPackage = require('./buildFilesFromPackageJson');
+//NGenerator.prototype.buildFilesFromPackage = require('./buildFilesFromPackageJson');
 
-NGenerator.prototype.createGithubProject = require('./createGitHubProject');
+ImaginamosGenerator.prototype.createGithubProject = require('./createGitHubProject');
